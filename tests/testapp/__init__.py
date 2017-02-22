6b7740e1ca2ed0505294802dc19cf9d7bd56ba33
Filip Wasilewski <en@ig.ma>
2017-02-22 02:10:12 +0100

    Split test script into project and app modules

---
+++ tests/testapp/__init__.py
@@ -0,0 +1,3 @@
+# coding=utf-8
+
+default_app_config = 'testapp.apps.TestsConfig'
---
+++ tests/testapp/views.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+
+from django.conf import settings
+from django.utils.timezone import now
+
+from easy_pdf.views import PDFTemplateView
+
+
+class DemoPDFView(PDFTemplateView):
+    template_name = 'hello.html'
+
+    base_url = 'file://{}/'.format(settings.STATIC_ROOT)
+
+    def get_context_data(self, **kwargs):
+        return super(DemoPDFView, self).get_context_data(
+            pagesize='A4',
+            title='Hi there!',
+            today=now(),
+            **kwargs
+        )
