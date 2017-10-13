c3f4ebc23d678259c03c7b33b2134b73a998619e
Bruno Nascimento <brunolnascimento@gmail.com>
2017-10-13 18:44:37 -0300

    fix uri for s3

--- easy_pdf/rendering.py
+++ easy_pdf/rendering.py
@@ -35,6 +35,8 @@ def fetch_resources(uri, rel):
         path = os.path.join(settings.STATIC_ROOT, uri.replace(settings.STATIC_URL, ""))
     elif settings.MEDIA_URL and uri.startswith(settings.MEDIA_URL):
         path = os.path.join(settings.MEDIA_ROOT, uri.replace(settings.MEDIA_URL, ""))
+    elif uri.startswith('https'):
+        path = uri
     elif settings.MEDIA_URL:
         path = settings.MEDIA_URL + '/' + uri
     else:
