2d7e7084919123e008af7703ab20c2d3a955305a
Cesar Abel <xangcastle@gmail.com>
2020-10-28 09:10:19 -0600

    Update rendering.py
    Adding support for python 3, deprecating django.utils.six in favor of six module

--- easy_pdf/rendering.py
+++ easy_pdf/rendering.py
@@ -9,7 +9,10 @@ from django.conf import settings
 from django.template import loader
 from django.http import HttpResponse
 from django.utils.http import urlquote
-from django.utils.six import BytesIO
+try:
+    from django.utils.six import BytesIO
+except:
+    from six import BytesIO
 
 import xhtml2pdf.default
 from xhtml2pdf import pisa
